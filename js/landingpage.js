d6a41c8e2d9b7fa2e336069e13f449cbe1b5cbb2
Alex Mendonca <142828601+alexmendonca07@users.noreply.github.com>
2024-03-11 17:06:16 +0000

    Mon, Mar 11, 2024, 10:06 AM -07:00

--- js/landingpage.js
+++ js/landingpage.js
@@ -3,15 +3,24 @@ open = document.getElementById('open')
 modal = document.getElementById('modal')
 close = document.getElementById('close')
 
-
+//toggle nav bar
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
 
+//show modal
 open.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
 
+//hide modal
 close.addEventListener('click', () => {
     modal.classList.remove('show-modal')
+})
+
+//hide modal on outside click
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
 })
